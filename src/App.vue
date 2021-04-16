663025c383e380d6df893e08bb62ad871c0f6fae
sumit-kushwah <sumitkushwah1729@gmail.com>
2021-04-16 19:04:48 +0530

    current time and ends in added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Time Pass logo" src="./assets/time-pass-logo.png">
+  <HelloWorld />
 </template>
 
 <script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,66 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1> Current Time </h1>
+    <h2>{{ time }}</h2>
+    <div class="row">
+      <div class="col-sm">
+        Year ends
+        <h2>{{ year }}</h2>
+      </div>
+      <div class="col-sm">
+        Month ends
+        <h2>{{ month }}</h2>
+      </div>
+      <div class="col-sm">
+        Week ends
+        <h2>{{ week }}</h2>
+      </div>
+      <div class="col-sm">
+        Day ends
+        <h2>{{ day }}</h2>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import moment from "moment";
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+  },
+  data() {
+    return { 
+      time: moment().format('LLLL'),
+      day: "",
+      week: "",
+      month: "",
+      year: "",
+      }
+  },
+  created() {
+    setInterval(() => {
+      this.time = moment().format('LLLL');
+      this.day = this.getDay();
+      this.week = this.getWeek();
+      this.month = this.getMonth();
+      this.year = this.getYear();
+    }, 1000)
+  },
+  methods: {
+    getYear() {
+      return moment().endOf('year').fromNow();
+    },
+    getMonth() {
+      return moment().endOf('month').fromNow();
+    },
+    getWeek() {
+      return moment().endOf('week').fromNow();
+    },
+    getDay() {
+      return moment().endOf('day').fromNow();
+    },
   }
 }
 </script>
